79f111e6e7648582631fef42a79503ada9367fcc
forSimpleTest <45491728+forSimpleTest@users.noreply.github.com>
2022-11-09 22:06:03 +0300

    Create test.py
    1

---
+++ test.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+import argparse
+import datetime
+import configparser
+
+def work():
+	passw = '1234567890oiuytrewqasdfbvc!=3'
+	print(passw)
+
+if __name__ == '__main__':
+	work()
